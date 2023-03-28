08dd64c072d28eef4fad0ced0657a12b844c5285
Bascom3000 <112181817+Bascom3000@users.noreply.github.com>
2023-03-28 11:58:45 +0200

    pokemonList exercice

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,31 @@
 function PokemonCard() {
-    return   <figure>
+
+
+  const pokemon = pokemonList[1]
+
+  return pokemon.imgSrc === true ? <figure>
     <img
-      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-      alt="bulbasaur artwork"
+      src={pokemon.imgSrc}
+      alt={pokemon.name}
     />
-    <figcaption>bulbasaur</figcaption>
-  </figure>;
-  }
-  
-  export default PokemonCard;
+    <figcaption>{pokemon.name}</figcaption>
+  </figure>                     : <figure>
+    <p>???</p>
+    <figcaption>{pokemon.name}</figcaption>
+  </figure>
 
+}
 
+export default PokemonCard;
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
