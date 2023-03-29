bc9b44159ec8b153240dae57cb55303211b288bd
Bascom3000 <112181817+Bascom3000@users.noreply.github.com>
2023-03-29 16:53:39 +0200

    state 1 edit

--- src/App.jsx
+++ src/App.jsx
@@ -10,29 +10,29 @@ import PokemonCard from "./components/PokemonCard"
 
 const pokemonList = [
   {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "charmander",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-    },
-    {
-      name: "squirtle",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-    },
-    {
-      name: "pikachu",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 
 
@@ -42,41 +42,49 @@ const pokemonList = [
 function App() {
   //states
   const [pokemonIndex, setPokemonIndex] = useState(0)
-  
+  const [disabledBtnPrevious, setDisabledBtnPrevious] = useState(false)
+  const [disabledBtnNext, setDisabledBtnNext] = useState(false)
   //manipulation
   let pokemon = pokemonList[pokemonIndex]
- 
-  const previously = () =>{
-  
-    if (pokemonIndex <= 0){
-      alert("Bulbasaur is the first") 
-    }else{
-      setPokemonIndex(pokemonIndex -1)
+
+
+
+
+  const previously = () => {
+
+    if (pokemonIndex <= 0) {
+
+      setDisabledBtnPrevious(true)
+    } else {
+      setDisabledBtnNext(false)
+      setPokemonIndex(pokemonIndex - 1)
     }
-  
-}
+
+  }
 
 
-  const next = () =>{
-  
-      if (pokemonIndex >= pokemonList.length - 1){
-        alert("There is no more pokemons")
-      }else{
-        setPokemonIndex(pokemonIndex + 1)
-      }
-    
+  const next = () => {
+
+    if (pokemonIndex >= pokemonList.length - 1) {
+
+      setDisabledBtnNext(true)
+    } else {
+      setDisabledBtnPrevious(false)
+      setPokemonIndex(pokemonIndex + 1)
+    }
+
   }
   //render
   return (
     <>
       <div>
-      <PokemonCard data={pokemon}/>
+        <PokemonCard data={pokemon} />
       </div>
-      <button onClick={previously}>Précédent</button>
-      <button onClick={next}>Suivant</button>
+      <button disabled={disabledBtnPrevious} onClick={previously}>Previous</button>
+      <button disabled={disabledBtnNext} onClick={next}>Next</button>
     </>
-    
-    
+
+
   );
 }
 
